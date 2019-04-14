46e06e41f946c88f53d5575cb6849e25f07691d4
root <root@lenovo14.lyclub.info>
2019-04-14 22:51:28 +0800

    version 1.1 toupiao_config.php

--- data/toupiao_config.php
+++ data/toupiao_config.php
@@ -9,16 +9,16 @@
 include("sqlite3.php");
 $db=new DataBase("sqlite3db/toupiao.db");
 
-/**tp_mulu*********************************************************************************************************************************************
-  tp_id    | tp_title          | tp_start     | tp_end       | tp_min        | tp_max         | tp_cuser           | tp_ctime        | tp_bakinfo 
-------------------------------------------------------------------------------------------------------------------------------------------------------
-投票目录号 | 投票问题的标题    | 投票开始时间 | 投票结束时间 | 投票最小选项  | 投票最大选项   | 投票发布用户别名   | 投票创建时间    | 投票备注信息
-******************************************************************************************************************************************************/
+/**tp_mulu***************************************************************************************************************************************************************************
+  tp_id    | tp_title       | tp_start     | tp_end       | tp_min       | tp_max       |  tp_view_stop    |     tp_pass    | tp_znum  |  tp_cuser    | tp_ctime     | tp_bakinfo 
+-------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+投票目录号 | 投票问题的标题 | 投票开始时间 | 投票结束时间 | 投票最小选项 | 投票最大选项 | 查看结果截止日期 | 投票者验证密码 | 投票总数 | 发布用户别名 | 投票创建时间 | 投票备注
+*************************************************************************************************************************************************************************************/
 $istb[0]="drop table tp_mulu";
-$istb[1]="create table tp_mulu(tp_id int unique,tp_title varchar(100) not null,tp_start date not null,tp_end date not null,tp_min int not null,tp_max int not null,tp_cuser varchar(50) not null,tp_ctime datetime not null,tp_bakinfo varchar(1000))";
-$istb[2]="insert into tp_mulu values(1000,'测试投票标题一','2019-02-01','2019-02-28','2','3','投票管理员','2019-01-31 08:32:33','')";
-$istb[20]="insert into tp_mulu values(1001,'测试已结束的投票标题','2019-02-01','2019-02-02','1','2','投票管理员','2019-01-31 08:32:33','')";
-$istb[21]="insert into tp_mulu values(1002,'测试未开始的投票标题','2029-03-01','2029-03-31','1','2','投票管理员','2019-01-31 08:32:33','')";
+$istb[1]="create table tp_mulu(tp_id int unique,tp_title varchar(100) not null,tp_start datetime not null,tp_end datetime not null,tp_min int not null,tp_max int not null,tp_view_stop datetime ,tp_pass varchar(100) not null,tp_znum int not null,tp_cuser varchar(50) not null,tp_ctime datetime not null,tp_bakinfo varchar(1000))";
+$istb[2]="insert into tp_mulu values(1000,'测试投票标题一','2019-02-01 00:00:00','2019-02-28 23:59:59','2','3','2019-04-30 23:59:59','".sha1("891021")."',4,'投票管理员','2019-01-31 08:32:33','')";
+$istb[20]="insert into tp_mulu values(1001,'测试已结束的投票标题','2019-04-12 00:00:00','2019-04-12 23:59:59','1','2','2019-04-30 23:59:59','".sha1("891021")."',5,'投票管理员','2019-01-31 08:32:33','')";
+$istb[21]="insert into tp_mulu values(1002,'测试未开始的投票标题','2029-03-01 00:00:00','2029-03-31 23:59:59','1','2','2019-04-30 23:59:59','".sha1("891021")."',0,'投票管理员','2019-01-31 08:32:33','')";
 /**tp_mlst********************************************************************************
   tp_id      |  tp_option          | tp_value          | tp_count    | tp_bakinfo
 ------------------------------------------------------------------------------------------
@@ -44,8 +44,8 @@ $istb[14]="insert into tp_mlst values(1002,'选项十','74',0,null)";
 *******************************************************************************************************************************************/
 $istb[15]="drop table tp_users";
 $istb[16]="create table tp_users(tpu_id int unique,tpu_name varchar(100) not null,tpu_alias varchar(200) not null,tpu_pass varchar(100) not null,tpu_admin bool  not null,tpu_tel varchar(12) not null,tpu_mail varchar(100) not null,tpu_bakinfo varchar(1000))";
-$istb[17]="insert into tp_users values(9000,'admin','投票管理员1','".sha1("123456")."',1,'18562221224','liubingjie771@live.cn',null)";
-$istb[18]="insert into tp_users values(9001,'liubingjie771','投票管理员2','".sha1("891021")."',1,'18562221224','liubingjie771@live.cn',null)";
+$istb[17]="insert into tp_users values(9000,'admin','投票管理员1','".sha1("891021")."',1,'18562221224','liubingjie771@live.cn',null)";
+$istb[18]="insert into tp_users values(9001,'liubingjie771','投票管理员2','".sha1("lbj*891021")."',1,'18562221224','liubingjie771@live.cn',null)";
 $istb[19]="insert into tp_users values(9002,'guest','临时访问者','".sha1("123")."',1,'18562221224','liubingjie771@live.cn',null)";
 
 for($i=0;$i<count($istb);$i++)
